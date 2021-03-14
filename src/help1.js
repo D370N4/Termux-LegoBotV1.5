9a7bab7ce104eb6dffa7761a05379cb264e01773
D370N4 <76952249+D370N4@users.noreply.github.com>
2021-03-14 06:22:02 -0300

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,46 @@
+const help1 = (prefix) => {
+
+	return `
+╭──────────────╮
+ *COMANDOS DO LegoBot*
+╰──────────────╯
+ 
+ ➸ *${prefix}grupo*
+➸ *${prefix}marcar*
+➸ *${prefix}marcar1*
+➸ *${prefix}marcar2*
+➸ *${prefix}marcar3*
+➸ *${prefix}loli1*
+➸ *${prefix}hentai*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}boanoite1*
+➸ *${prefix}bomdia1*
+➸ *${prefix}boatarde1*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}akeno*
+➸ *${prefix}meme*
+➸ *${prefix}lofi*
+➸ *${prefix}malkova*
+➸ *${prefix}nsfwloli*
+➸ *${prefix}reislin*
+➸ *${prefix}limpar*
+➸ *${prefix}marcar*
+➸ *${prefix}ts (texto que deseja transmitir)*
+
+╔════════════════════
+  Bot do Legosi-dono
+  DUVIDAS? 👇
+  wa.me/18482765528
+╚════════════════════`
+
+}
+exports.help1 = help1
