1ab1d5611e421d4dd891fca05b7b9327f8c591af
D370N4 <76952249+D370N4@users.noreply.github.com>
2021-03-21 03:22:54 -0300

    Add files via upload

---
+++ src/idiome.js
@@ -0,0 +1,69 @@
+const idiome = (prefix) => {
+	return `
+
+╔════════════════════
+║        🌹LegoBot V1🌹
+╠════════════════════
+
+Aqui estão todos os idiomas disponíveis
+
+➸ escreva "af" para "Afrikaans"
+➸ escreva "sq" para "Albanês"
+➸ escreva "ar" para "Árabe"
+➸ escreva "hy" para "Armênio"
+➸ escreva "ca" para "Catalão"
+➸ escreva "zh" para "Chinês"
+➸ escreva "zh-cn" para "Chinês (Mandarim/China)"
+➸ escreva "zh-tw" para "Chinês (Mandarim/Taiwan)"
+➸ escreva "zh-yue" para "Chinês (Cantonesa)"
+➸ escreva "hr" para "Croata"
+➸ escreva "cs" para "Tcheco"
+➸ escreva "da" para "Dinamarquês"
+➸ escreva "nl" para "Holandês"
+➸ escreva "en" para "Inglês"
+➸ escreva "en-au" para "Inglês (Australia)"
+➸ escreva "en-uk" para "Inglês (United Kingdom)"
+➸ escreva "en-us" para "Inglês (United States)"
+➸ escreva "eo" para "Esperanto"
+➸ escreva "fi" para "Finlandês"
+➸ escreva "fr" para "Francês"
+➸ escreva "de" para "Alemão"
+➸ escreva "el" para "Grego"
+➸ escreva "ht" para "Crioulo haitiano"
+➸ escreva "hi" para "Hindi"
+➸ escreva "hu" para "Húngaro"
+➸ escreva "is" para "Islandês"
+➸ escreva "id" para "Indonésio"
+➸ escreva "it" para "Italiano"
+➸ escreva "ja" para "Japonês"
+➸ escreva "ko" para "Coreano"
+➸ escreva "la" para "Latina"
+➸ escreva "lv" para "Letão"
+➸ escreva "mk" para "Macedônio"
+➸ escreva "no" para "Norueguês"
+➸ escreva "pl" para "Polonês"
+➸ escreva "pt" para "Português"
+➸ escreva "pt-br" para "Português (Brasileiro)"
+➸ escreva "ro" para "Romena"
+➸ escreva "ru" para "Russo"
+➸ escreva "sr" para "Sérvio"
+➸ escreva "sk" para "Eslovaco"
+➸ escreva "es" para "Espanhol"
+➸ escreva "es-es" para "Espanhol (Espanha)"
+➸ escreva "es-us" para "Espanhol (Estados Unidos)"
+➸ escreva "sw" para "Suaíli"
+➸ escreva "sv" para "Sueco"
+➸ escreva "ta" para "Tâmil"
+➸ escreva "th" para "Tailandês"
+➸ escreva "tr" para "Turco"
+➸ escreva "vi" para "Vietnamita"
+➸ escreva "cy" para "Welsh"
+
+╔════════════════════
+  FEITO POR *Legosi-dono*
+  DUVIDAS? 👇
+  wa.me/18482765528
+╚════════════════════`
+}
+
+exports.idiome = idiome
