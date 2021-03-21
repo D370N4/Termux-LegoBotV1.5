9975e396c5ebba246f711f8732e893b221039763
D370N4 <76952249+D370N4@users.noreply.github.com>
2021-03-21 03:47:58 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,109 @@
+const help = (prefix) => {
+	return `
+
+╔════════════════════
+║        🌹LegoBot V1🌹
+╠════════════════════
+
+
+➸ Prefix:  *「${prefix} 」*
+➸ Status: *「 Online 」*
+
+       • ──── ✾ ──── •
+       *FIGURINHAS*【✔】
+       • ──── ✾ ──── •
+      
+➸ Comando : *${prefix}sticker*, *${prefix}stiker*, *${prefix}figurinha* ou *${prefix}figu*
+➸ útil em : converter imagem/gif/vídeo em adesivo
+➸ uso : responder imagem/gif/video ou enviar imagem/gif/video com legenda\n
+➸ Comando : *${prefix}toimg*
+➸ útil em : converter adesivo em imagem
+➸ uso : adesivo de resposta\n
+
+       • ─── ✾ ─── •
+       *MEMES*【✔】
+       • ─── ✾ ─── •
+      
+➸ Comando : *${prefix}meme*
+➸ útil em : mandar imagens aleatórias de meme [inglês]
+➸ uso : basta emviar o comando\n
+
+       • ──── ✾ ──── •
+       *OUTROS...*【✔】
+       • ──── ✾ ──── •
+       
+➸ Comando : *${prefix}grupo*
+➸ útil em : apoia o criador do bot e  ganha um belo grupo de hentai  de brinde:D
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}tts (sigla de idioma) [frase]*
+➸ útil em : ele manda tudo que você escreve em áudio
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}lolifoda*
+➸ útil em : mandar a imagem da capa do grupo Lolis & Lolicons
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}superpack*
+➸ útil em : pack gigantesco de links do superhentais, escolha entre 1 a 5
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}donate*
+➸ útil em : apoia o criador do bot pelo Kwai
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}wait*
+➸ útil em : pesquisar sobre o anime por imagem [ Que anime é este/que ]
+➸ uso : responder imagem ou enviar imagem com legenda\n
+➸ Comando : *${prefix}setprefix*
+➸ útil em : alterar o prefixo do bot
+➸ uso : *${prefix}setprefix [texto|opcional]*\nexemplo : *${prefix}setprefix ?*
+➸ Nota : Usado somente pelo proprietário do bot\n
+
+       • ─── ✾ ─── •
+       *GRUPO*【✔】
+       • ─── ✾ ─── •
+      
+➸ Comando : *${prefix}linkgrupo*
+➸ útil em : enviar o link do grupo
+➸ Comando : *${prefix}listadm*
+➸ útil em : mostra todos os adms do grupo
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}infogrupo*
+➸ útil em : mostra o nome e a descriçao do grupo
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}marcar*
+➸ útil em : marcar todos os membros do grupo, incluindo administradores
+➸ uso : ao enviar você pode escolher o formato entre 1 a 3, ou continuar com o convencional\n
+➸ Nota : Você precisa ser administrador do grupo\n
+➸ Comando : *${prefix}onlyadm*
+➸ útil em : enviar o link do grupo
+➸ uso : fecha o grupo ao colocar 0 e abre o grupo quando colocar 1\n
+➸ Nota : O bot precisa ser admin\n
+➸ Comando : *${prefix}add*
+➸ útil em : adicionar membro ao grupo
+➸ uso : *${prefix}add 5511xxxxx*\n
+➸ Nota : o bot precisa ser admin!\n
+➸ Comando : *${prefix}kick*
+➸ útil em : remover membros do grupo
+➸ uso : *${prefix}kick e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+➸ Comando : *${prefix}promote*
+➸ útil em : tornar membro do grupo um administrador
+➸ uso : *${prefix}promote e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+➸ Comando : *${prefix}demote*
+➸ útil em : tornar o administrador um membro comum
+➸ uso : *${prefix}demote e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+
+       • ────── ✾ ────── •
+       *MENU DO LegoBot*【✔】
+       • ────── ✾ ────── •              
+
+➸ *${prefix}help1* ♔
+    
+
+╔════════════════════
+  FEITO POR *Legosi-dono*
+  DUVIDAS? 👇
+  wa.me/18482765528
+╚════════════════════`
+}
+
+exports.help = help
